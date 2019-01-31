8ec2c0865c6fb36c6652cbd27309368d310f92bd
umar frog <omar.hambaran@frogasia.com>
2019-01-31 16:42:07 +0800

    ready for training

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 0,
+      b: 0,
+      data: null
+    };
+    this.onUbahAyat = this.onUbahAyat.bind(this);
+    this.getDataFromServer = this.getDataFromServer.bind(this);
+  }
+
+  onUbahAyat(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  getDataFromServer() {
+    let data = {
+      a: this.state.a,
+      b: this.state.b
+    };
+    fetch('http://localhost:8081/calcAdd', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(res => {
+      alert(JSON.stringify(res));
+      console.log(res);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <span>
+        A: <input type="text" name="a" value={this.state.a} onChange={this.onUbahAyat} /> <br />
+        B: <input type="text" name="b" value={this.state.b} onChange={this.onUbahAyat} />
+        <button type="button" onClick={this.getDataFromServer}>Calculate Us</button>
+        <h3>{this.state.data}</h3>
+      </span>
     );
   }
 }
